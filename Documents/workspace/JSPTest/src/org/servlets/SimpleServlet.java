165d22be7c1a3585727dc414925dc1f3d30886e6
ahlo <lo.drew24@gmail.com>
2015-08-05 20:38:53 -0400

    index.html and SimpleServlet.java created.

---
+++ Documents/workspace/JSPTest/src/org/servlets/SimpleServlet.java
@@ -0,0 +1,29 @@
+package org.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet(description = "Simple servlet", urlPatterns = { "/SimpleServlet" })
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from Servlet GET method.");
+		response.getWriter().print("Simple Servlet: Hello - on the web page.");
+		
+	}
+
+}
